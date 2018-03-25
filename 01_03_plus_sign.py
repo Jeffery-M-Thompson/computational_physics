db3272118eb2c9403cfde1a4aa080e7cdcb2a18c
Jeffery M. Thompson <jthompson@svc.edu>
2018-03-25 12:24:13 -0400

    Added scaled axis

--- 01_03_plus_sign.py
+++ 01_03_plus_sign.py
@@ -5,4 +5,8 @@ import matplotlib.pyplot as plt
 
 plt.plot((0, 0), (-10, 10), 'k-')
 plt.plot((-10, 10), (0, 0), 'k-')
+plt.axis('scaled')
+plt.xlim(-20, 20)
+plt.ylim(-20, 20)
+plt.title('Plus sign.')
 plt.show()
