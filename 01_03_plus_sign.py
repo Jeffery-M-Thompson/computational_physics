9696fefd93bf824e83f019bbe3f1285455ff2992
Jeffery M. Thompson <jthompson@svc.edu>
2018-03-25 01:46:14 -0400

    Added Program 01-03

--- 01_03_plus_sign.py
+++ 01_03_plus_sign.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
+plt.plot((0, 0), (-10, 10), 'k-')
+plt.plot((-10, 10), (0, 0), 'k-')
+plt.show()
