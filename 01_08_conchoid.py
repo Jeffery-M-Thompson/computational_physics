a548a1b76709d3971e89fda5a5f00b04484db03c
Jeffery M. Thompson <jthompson@svc.edu>
2018-04-06 13:13:45 -0400

    Added detail to legend

--- 01_08_conchoid.py
+++ 01_08_conchoid.py
@@ -9,20 +9,20 @@ def conchoid(b):
 	r1 = 1.0/np.sin(theta)+a
 	r2 = 1.0/np.sin(theta)-a
 	gc= colors[b]
-	labels = str(a)
+	labels = 'b = '+str(a)
 	ax.plot(theta, r1, color = gc, label = labels)
 	ax.plot(theta, r2, color = gc)
 	return
 
 colors = ['purple', 'b', 'g', 'y', 'orange', 'r']
 ax = plt.subplot(111, projection='polar')
-for b in list(range(6)):
+for b in list(range(5)):
 	conchoid(b)
 ax.set_rmax(3)
 ax.set_rticks([0.5,1,1.5,2,2.5,3])
 ax.set_rlabel_position(-20)
 ax.grid(True)
-ax.set_title("Conchoid of Nicomedes")
+ax.set_title(r'Conchoid of Nicomedes $\csc \theta \pm b$')
 legend = ax.legend(loc='lower left', shadow=True)
 frame = legend.get_frame()
 frame.set_facecolor('0.90')
