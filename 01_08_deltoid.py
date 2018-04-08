fe6a3388032282076d30c2e207e94af81b57371b
Jeffery M. Thompson <jthompson@svc.edu>
2018-04-08 13:44:31 -0400

    Added deltoid

---
+++ 01_08_deltoid.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+theta = np.arange(0, 2*np.pi, 0.001)
+x = 2*a*np.cos(theta)+a*np.cos(2*theta)
+y = 2*a*np.sin(theta)-a*np.sin(2*theta)
+ax.plot(x, Y)
+ax.set_title("Deltoid")
+plt.show()
