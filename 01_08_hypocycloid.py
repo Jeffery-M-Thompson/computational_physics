c96a3ba4254b104a8d253e9018a96bbccd8c69b1
Jeffery M. Thompson <jthompson@svc.edu>
2018-04-22 13:21:17 -0400

    Fixed b in folium and added hypocucloid

--- 01_08_hypocycloid.py
+++ 01_08_hypocycloid.py
@@ -4,25 +4,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def folium(b):
+def hypocycloid(b):
 	theta = np.arange(0, 2*np.pi, 0.001)
 	a = float(b * 0.5 + 0.5)
-	r = (3*a*np.sin(theta)*np.cos(theta))/((np.sin(theta))**3+(np.cos(theta)**3))
+	
 	gc= colors[b]
 	labels = 'a = '+str(a)
-	ax.plot(theta, r, color = gc, label = labels)
+	x = a * (np.cos(theta))**3
+	y = a * (np.sin(theta))**3
+	ax.plot(x, y, color = gc, label = labels)
 	return
 
 colors = ['purple', 'b', 'g', 'y', 'orange', 'r']
-ax = plt.subplot(111, projection='polar')
+ax = plt.subplot(111)
 for b in list(range(5)):
-	folium(b)
-ax.set_rmax(7)
-ax.set_rticks([1,2,3,4,5,6,7])
-ax.set_rlabel_position(-20)
-ax.grid(True)
+	hypocycloid(b)
 ax.set_title(
-	r'Folum of Descarte $\frac{3a\sin{\theta}\cos{\theta}}{\sin^3{\theta}+\cos^3{\theta}}$')
+	r'Hypocycloid $x^{2/3}+y^{2/3}=a^{2/3}$')
 legend = ax.legend(loc='lower left', shadow=True)
 frame = legend.get_frame()
 frame.set_facecolor('0.90')
